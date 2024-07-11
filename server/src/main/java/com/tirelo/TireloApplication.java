b93b4b978de79927de0917f0d68b2012a7f9ba3d
7irelo <tirelo.eric@gmail.com>
2024-07-11 09:40:50 +0200

    Spring Boot Server

---
+++ server/src/main/java/com/tirelo/TireloApplication.java
@@ -0,0 +1,13 @@
+package com.tirelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TireloApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/tirelo/TireloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TireloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
